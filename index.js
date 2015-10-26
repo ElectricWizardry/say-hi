fa141eb1f8c1b3afbd4272121ac5aeab8081d2f3
Eric Bailey <eric@ericb.me>
2015-10-26 15:13:34 -0500

    Make it literate
    ... because, why not?

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ exports = module.exports = function(name) {
     if (req.method !== 'GET' || req.url !== '/status')
       return next()
 
-    var diff = process.hrtime(START),
-      message = 'hi',
-      uptime = Math.floor((diff[0] * 1e9 + diff[1]) / 1e6);
+    var diff    = process.hrtime(START),
+        message = 'hi',
+        uptime  = Math.floor((diff[0] * 1e9 + diff[1]) / 1e6);
     if (name && typeof name === 'string' && name.length)
       message += ' from ' + name
 
